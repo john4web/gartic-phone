e1af3e20650ead77039c26d3b0bfeb347dfa3991
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2021-05-07 11:56:05 +0200

    adding router paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './components/book/book.component';
+import { DrawComponent } from './components/draw/draw.component';
+import { HomeComponent } from './components/home/home.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { StartComponent } from './components/start/start.component';
+import { WriteComponent } from './components/write/write.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'lobby',
+    component: LobbyComponent,
+  },
+  {
+    path: 'start',
+    component: StartComponent,
+  },
+  {
+    path: 'draw',
+    component: DrawComponent,
+  },
+  {
+    path: 'write',
+    component: WriteComponent,
+  },
+  {
+    path: 'book',
+    component: BookComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { StartComponent } from './components/start/start.component';
+import { DrawComponent } from './components/draw/draw.component';
+import { WriteComponent } from './components/write/write.component';
+import { BookComponent } from './components/book/book.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StartComponent,
+    DrawComponent,
+    WriteComponent,
+    BookComponent,
+    LobbyComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
